47aed4446a514ba39db4d23834d60dd36e551972
Piedad García <piedad.pgo@gmail.com>
2025-04-24 21:38:32 +0200

    Mejorado navbar-collapse

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,67 +1,60 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <!-- Botón para dispositivos pequeños -->
+<nav class="navbar navbar-expand-lg navbar-secondary bg-body-tertiary">
+    <div class="container">
+        <a href="{{ route('inicio') }}" class="navbar-brand">
+            <img src="{{ asset('images/game_crush.jpg') }}" alt="Logo" height="40">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Menú de navegación">
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <!-- Navegación izquierda -->
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link border-end border-2 px-3" href="{{ route('inicio') }}">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link border-end border-2 px-3" href="{{ route('mostrar-videojuegos') }}">Ver</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link border-end border-2 px-3" href="{{ route('anadir') }}">Añadir</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link border-end border-2 px-3" href="{{ route('videojuego.edit.select') }}">Editar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link border-end border-2 px-3"
+                        href="{{ route('videojuego.delete.select') }}">Borrar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link border-end border-2 px-3" href="{{ route('generos') }}">Géneros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="{{ route('sobre-nosotros') }}">Sobre nosotros</a>
+                </li>
+            </ul>
 
-        <!-- Contenedor del menú -->
-        <div class="collapse navbar-collapse d-flex flex-wrap" id="navbarNav">
-            <div class="container nav me-auto">
-                <a href="{{ route('inicio') }}"
-                    class="mb-2 me-3 mb-lg-0 text-white text-decoration-none">
-                    <img src="{{ asset('images/game_crush.jpg') }}" alt="Logo" width="auto" height="40"></a>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link border-end border-2 px-3" href="{{ route('inicio') }}">Inicio</a>
+            <!-- Navegación derecha -->
+            <ul class="navbar-nav ms-auto">
+                @auth
+                    <li class="nav-item border-end border-2 px-3">
+                        <p class="mb-0 text-white">{{ Auth::user()->name }}</p>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link border-end border-2 px-3" href="{{ route('mostrar-videojuegos') }}">Ver</a>
+                    <li class="nav-item ms-3">
+                        <form method="POST" action="{{ route('usuario.logout') }}">
+                            @csrf
+                            <button type="submit" class="btn btn-outline-light btn-sm">Cerrar sesión</button>
+                        </form>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link border-end border-2 px-3" href="{{ route('anadir') }}">Añadir</a>
+                @else
+                    <li class="nav-item ms-auto">
+                        <a class="nav-link border-end border-2 px-3" href="{{ route('usuario.login') }}">Iniciar sesión</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link border-end border-2 px-3"
-                            href="{{ route('videojuego.edit.select') }}">Editar</a>
+                    <li class="nav-item ms-auto">
+                        <a class="nav-link px-3" href="{{ route('usuario.register') }}">Registrar usuario</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link border-end border-2 px-3"
-                            href="{{ route('videojuego.delete.select') }}">Borrar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link border-end border-2 px-3" href="{{ route('generos') }}">Géneros</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link px-3" href="{{ route('sobre-nosotros') }}">Sobre
-                            nosotros</a>
-                    </li>
-                </ul>
-
-                <ul class="navbar-nav ms-auto my-auto align-items-center">
-                    @auth
-                        <li class="nav-item border-end border-2 px-3 my-auto ">
-                            <p class="my-auto"> {{ Auth::user()->name }} </p>
-                        </li>
-                        <li class="nav-item mx-3 my-auto">
-                            <form method="post" action="{{ route('usuario.logout') }}" class="my-auto">
-                                @csrf
-                                <button type="submit" class="logout btn btn-outline-dark btn-sm">Cerrar sesión</button>
-                            </form>
-                        </li>
-                    @else
-                        <li class="nav-item my-auto">
-                            <a class="nav-link border-end border-2 px-3 my-auto" href="{{ route('usuario.login') }}">Iniciar
-                                sesión</a>
-                        </li>
-                        <li class="nav-item my-auto">
-                            <a class="nav-link px-3 my-auto" href="{{ route('usuario.register') }}">Registrar usuario</a>
-                        </li>
-                    @endauth
-                </ul>
-
-            </div>
-
+                @endauth
+            </ul>
+        </div>
+    </div>
 </nav>
